b826515f0e0b75a5caaf3cfae011bc3ad5b6d6f7
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-07-01 14:52:18 -0700

    commit tree 9ae65d30aacae83f6829f77cefacd070719ef3ff

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+     let x = document.querySelector('#text')
+     x.innerHTML = "This is really cool!"
+     return ;
+  })
+
+
+  
+
+
